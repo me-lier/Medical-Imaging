e34bb627baf9712fcaf70de5b6edad00e767f183
Srinivas Deekonda <126309422+me-lier@users.noreply.github.com>
2024-07-20 06:05:57 +0530

    Delete Project directory

--- Project/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const contactLink = document.querySelector('a[href="#contact"]');
-
-    contactLink.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector('#contact').scrollIntoView({ behavior: 'smooth' });
-    });
-});
